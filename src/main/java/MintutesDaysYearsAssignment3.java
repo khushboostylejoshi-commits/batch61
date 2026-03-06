3c292672b8e6b7cf641f37251921a3165eb1b0a1
khushboostylejoshi-commits <khushboostylejoshi@gmail.com>
2026-03-06 19:10:35 +0530

    TimeConversion Assignment

--- src/main/java/MintutesDaysYearsAssignment3.java
+++ src/main/java/MintutesDaysYearsAssignment3.java
@@ -6,19 +6,22 @@ public class MintutesDaysYearsAssignment3 {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Prove the no. of mins");
+        System.out.println("Provide the number ");
         //Using long as the memory space for long is 8bytes ,bigger than the memory space of int (4bytes)
-        long mins = scanner.nextLong();
-
+        long totalsecond = scanner.nextLong();
+        long second = totalsecond%60;
+        long minutes = (totalsecond%3600)/60 ;
         //To convert min into days , one ay has 24 hrs and one hr has 60 min ,so dividing the mins by 60*24
-
-        long days = mins/(24*60);
+       long hour = totalsecond/3600;
+        long days = minutes/(24*60);
         System.out.println("No. od days are as follow "+days+"");
 
         // To convert min into days dividing the value with 24*60*365 ,as we already have th days conversion ,for years we only have to divide by 365
 
         long years = days/365;
         System.out.println("No. of years "+years+"");
+
+        System.out.println("days"+days+" hour "+hour+" minutes "+minutes+" seconds "+second+"");
 
 
 
